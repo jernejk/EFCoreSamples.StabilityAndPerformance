dbeccb08e1f1272f1f5f5f61265dd130ffca56eb
Jernej Kavka (JK) <JernejKavka@ssw.com.au>
2021-09-01 08:26:45 +1000

    ✨ Added no operation (max possible perf) stats.

---
+++ EFCoreSamples.StabilityAndPerformance.Api/Controllers/NopController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreSamples.StabilityAndPerformance.Api.Controllers
+{
+    /// <summary>
+    /// Testing how fast can Web API react without any code.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class NopController : Controller
+    {
+
+        [HttpGet]
+        public void Ping()
+        {
+        }
+    }
+}
