a7c65f17866d3e4df06c2dacf8bc04bba6252897
Jernej Kavka (JK) <JernejKavka@ssw.com.au>
2023-09-25 22:10:25 +1000

    Updated Program.cs

--- EFCoreSamples.StabilityAndPerformance.Api/Program.cs
+++ EFCoreSamples.StabilityAndPerformance.Api/Program.cs
@@ -1,26 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EFCoreSamples.StabilityAndPerformance.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
 
-namespace EFCoreSamples.StabilityAndPerformance.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// By default we are adding SQL Server DB context.
+builder.Services.AddDbContextPool<SalesDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // You can also use SQL Server.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesDB"));
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+#if DEBUG
+    // Most project shouldn't expose sensitive data, which is why we are
+    // limiting to be available only in DEBUG mode.
+    // If this is not, SQL "parameters" will be '?' instead of actual values.
+    options.EnableSensitiveDataLogging();
+#endif
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
